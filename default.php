7fceb8468775c8eb1cc949ce99a33721d424731b
jrodhead <jaredheadrick@Jareds-iMac.local>
2015-07-22 15:35:03 -0500

    Content and Styles
    Added home page content and sass style guide

--- default.php
+++ default.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<!-- this file needs to be saved without BOM in Notepad ++ or TextWrangler. See for more info:
+<!-- this file needs to be saved without BOM. See for more info:
 http://stackoverflow.com/questions/10199355/php-include-causes-white-space-at-the-top-of-the-page -->
 <head>
 <meta charset="utf-8">
 <meta property="og:title" content="Jared Headrick" />
-<meta property="og:type" content="UI and Web Design" />
+<meta property="og:type" content="Product Design" />
 <meta property="og:url" content="http://www.jaredheadrick.com" />
 <meta property="og:image" content="" />
 <meta property="og:site_name" content="Jared Headrick" />
-<meta name="keywords" content="jared headrick, web design, ui design, lincoln, nebraska" />
+<meta name="keywords" content="jared headrick, product design, ux, ui, web, design, lincoln, nebraska" />
 <meta content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" name="viewport">
 <meta name = "format-detection" content = "telephone=no">
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
@@ -25,20 +25,73 @@ http://stackoverflow.com/questions/10199355/php-include-causes-white-space-at-th
 </style>
 <h1>Sorry, but your browser is old</h1>
 <p class="error">This site uses the latest tech that the internet has to offer. Your browser does not. We recommend updating your browser - everybody on the internets will thank you.</p>
-<p>If you can't update your browser, you can still <a href="http://www.facebook.com/northofneptune">find us on Facebook</a>!</p>
 <![endif]-->
-<div class="page">
 <header role="banner">
 	<h1>Jared Headrick</h1>
+	<p>Product Design</p>
 </header>
-
+<!--
+<nav>
+	<ul>
+		<li>about</li>
+		<li>process</li>
+		<li>work</li>
+		<li>contact</li>
+</nav>
+-->
 <section role="main">
-	<article class="shows">
-		<h1>Step 1: Setup</h1>
-        <p>Just making sure everything works. I'll have this looking good soon enough.</p>
+	<article>
+		<h1>About</h1>
+        <p>
+					I am a product designer focusing on UX, UI, and front-end design for web and mobile apps. I believe that designing a great solution requires a thorough understanding of the problem and the best way to come to that understanding is by getting to know your users and their process.
+				</p>
+				<p>
+					For the last eight years, I have worked many roles for a variety of teams and products - from leading a support/front-end web design team through defining requirements as a business analyst to acting as product owner for web and iOS app developers. This experience has put me in a position to understand what it takes to deliver a product that does what is necessary to succeed.
+				</p>
 	</article>
+	<article>
+		<h1>Process</h1>
+		<p>
+			The tools depend on the job, and knowing when to use the correct tool is a skill in itself. My toolbox is constantly expanding, but regardless of the job, I have a clear process for designing a solution: strategy, research and analysis, design, and iteration.
+		</p>
+		<div class="card">
+			<img alt="strategy icon" src="assets/strategy.png">
+			<h1>Strategy</h1>
+			<p>
+				I strive to understand why something is needed before anything else. I understand that a product cannot be all things to all people and that a clear product vision is essential to success. To get there, I’ll create personas, vision statements, key functions and characteristic tradeoffs, mood boards, affinity diagrams.
+			</p>
+		</div>
+		<div class="card">
+			<img alt="analysis icon" src="assets/analysis.png">
+			<h1>Analysis</h1>
+			<p>
+				The next step is to gather and digest any useful data that will inform the direction to take the solution. This includes user stories, business requirements, stakeholder interviews, current and future state analysis, process maps, system context diagrams, site maps.
+			</p>
+		</div>
+		<div class="card">
+			<img alt="design icon" src="assets/design.png">
+			<h1>Design</h1>
+			<p>
+				The really fun stuff - this is where I take all of the information I've gathered and start producing ideas. These can be in the form of sketches, wireframes, mockups, prototypes, HTML, CSS, jQuery, Interface Builder.
+			</p>
+		</div>
+		<div class="card">
+			<img alt="iterate icon" src="assets/iterate.png">
+			<h1>Iterate</h1>
+			<p>
+				It is rare to achieve perfection on the first try. Whether it is through reviews and feedback from my teammates or clients and users, the most important step is to review and revise. This includes review meetings, user acceptance testing, retrospectives.
+			</p>
+		</div>
+	</article>
+<!--
+	<article>
+		<h1>Work</h1>
+		<p>
+			I am not currently taking on projects, but if you’d like to see my portfolio, please contact me on <a href="https://www.linkedin.com/pub/jared-headrick/b9/974/58b">LinkedIn</a>.
+		</p>
+	</article>
+-->
 </section>
-
 <footer>
 <p role="contentinfo">&copy; <?php echo date("Y") ?> Jared Headrick</p>
 </footer>
